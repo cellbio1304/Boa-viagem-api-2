db88bd083194f39a7b9fdd234b83bc84bc334793
guicolas <email@test.com>
2022-04-11 08:52:20 -0300

    Projeto boa viagem atualizado

--- src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
+++ src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
@@ -2,6 +2,10 @@ package br.com.etechoracio.boa_viagem.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +16,25 @@ import br.com.etechoracio.boa_viagem.repository.GastoRepository;
 @RestController
 @RequestMapping("/gastos")	
 public class GastoController {
-
+	
+    @Autowired
 	private GastoRepository repository;
 	
+    @GetMapping
 	public List<Gasto> listarTodos(){
 		return repository.findAll();
 	}
+    
+    @GetMapping("/{id}")
+    public Gasto buscarPorId(@PathVariable long id) {
+    	return repository.findById(id).orElse(null);
+    	
+    }
+    
+    @DeleteMapping("/{id}")
+    public void deletarPorId(@PathVariable long id) {
+    	repository.deleteById(id);
+    	
+    }
+    
 }
